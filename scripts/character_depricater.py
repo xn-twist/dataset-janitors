15c6a446ecf1515eee8f686fadb0ef4463f1fb3a
Floyd Hightower <floyd.hightower27@gmail.com>
2017-09-06 21:40:59 -0300

    Adding script to remove characters from suggested_deprecations and move them to the deprecated branch

---
+++ scripts/character_depricater.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Removed characters suggested for deprecation from the list of possible spoofs."""
+
+from xntwist_python_sdk import xntwist_python
+
+# assume there is a configuration file in the current directory
+CONFIG_FILE_PATH = "./xn.conf"
+CHARACTER_DEPRICATION_THRESHOLD = 10
+
+
+def main():
+    """Move characters from the feed to the mappings."""
+    # instantiate an instance of the XN-Twist Python SDK
+    xn_sdk = xntwist_python.XnTwistSDK(CONFIG_FILE_PATH)
+
+    # get the characters that have been suggested for deprecation
+    suggestions = xn_sdk.get_branch("suggested_deprecations")['_items']
+    suggested_deprecation_count = {}
+
+    non_basic_characters = xn_sdk.get_branch("non_basic_characters")['_items']
+
+    for suggestion in suggestions:
+        if suggested_deprecation_count.get(suggestion['character']):
+            suggested_deprecation_count[suggestion['character']] += 1
+        else:
+            suggested_deprecation_count[suggestion['character']] = 1
+
+    for key in suggested_deprecation_count:
+        # if the character has been voted for deprecation too many times...
+        if suggested_deprecation_count[key] > CHARACTER_DEPRICATION_THRESHOLD:
+            # add the character to the list of deprecated characters
+            xn_sdk.add_item({"deprecated_character": key},
+                            "deprecated_characters")
+
+            # delete the character from the list of suggested deprecations
+            for suggestion in suggestions:
+                # if the character in the suggestion is the one we are deprecating, remove it from the list of suggestions
+                if suggestion['character'] == key:
+                    xn_sdk.delete(suggestion, "suggested_deprecations")
+
+            # delete the character from the list of non_basic_characters
+            for char in non_basic_characters:
+                if char['potential_spoof'] == key:
+                    xn_sdk.delete(char, "non_basic_characters")
+                    break
+
+
+if __name__ == '__main__':
+    main()
